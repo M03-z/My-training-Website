6f5ce9358d47c30bcbaa552d714c0c5191d67580
Mamon Taher <mamoun.taher1993@hotmail.com>
2020-12-16 23:12:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-var Adblock = prompt("Please Disable your adblocker and write adblocker to confirm", "adblocker");
+/* var Adblock = prompt("Please Disable your adblocker and write adblocker to confirm", "adblocker");
 if (Adblock !== adblocker) {
 Adblock= prompt('Write again');}
 else if (Adblock === adblocker) {
 console.log('New user choice: ' + userChoice);}
 
 var name = prompt("Please enter your name", "Name");
-alert("Welcome to our website "+name)
+alert("Welcome to our website "+name) */
 
+var BgImg = prompt('enter a number');
+  BgImg = parseInt(BgImg);
+  while(isNaN(BgImg)){
+    BgImg = prompt('enter a number');}
+    function userRequest(){
+        var data = '';
+        var i =0;
+        for(i=0; i<userInput; i++){
+            generateImage()
+        }
+        function generateImage(){
+            data += ' <img src="bg.jpg" />'
+      }
+      return data;
+    }
 /*
 var today= new Date();
 var hourNow = today.getHours();
